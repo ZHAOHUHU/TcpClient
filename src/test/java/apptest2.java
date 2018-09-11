aaf9696cfdc39fd4a38c3c2ebfbf47d9821a6ade
ZHAOHUHU <3066918701@qq.com>
2018-09-11 18:46:36 +0800

    gxfhdfh

--- src/test/java/apptest2.java
+++ src/test/java/apptest2.java
@@ -4,7 +4,7 @@
  * @author: Zhao Hong Ning
  * @create: 2018-09-10 11:06
  **/
-public class apptest2 {
+public class CutValue {
     public static double cutValue(String args) {
         String ss = "Id=810000011,Name=....,BaseTypeId=1006001001,Value=25,Valid=1,SampleTime=2018-09-10 11:02:33";
         String s = " Id=810000011,Name=....,BaseTypeId=1006001001,Value=25,Valid=1,SampleTime=2018-09-10 11:02:33.Id=810000021,Name=....,BaseTypeId=1006003001,Value=54.3,Valid=1,SampleTime=2018-09-10 11:02:33.";
@@ -15,9 +15,17 @@ public class apptest2 {
 
     }
 
-    public static void main(String[] args) {
-        String ss = "Id=810000011,Name=....,BaseTypeId=1006001001,Value=25,Valid=1,SampleTime=2018-09-10 11:02:33";
-        new apptest2().cutValue(ss);
+    public static double[] cutDoubleValue(String args) {
+        double[] temp=new double[2];
+        String s = " Id=810000011,Name=....,BaseTypeId=1006001001,Value=25,Valid=1,SampleTime=2018-09-10 11:02:33.Id=810000021,Name=....,BaseTypeId=1006003001,Value=54.3,Valid=1,SampleTime=2018-09-10 11:02:33.";
+        String[] split = s.split("Value=");
+        String[] strings = split[1].split(",");
+        String[] stringss = split[2].split(",");
+        temp[0]= Double.parseDouble(strings[0]);
+        temp[1]= Double.parseDouble(stringss[0]);
+return  temp;
+
+
     }
 
 }
